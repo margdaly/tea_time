9f6a0f71493471be3d6be793ec258fa91b27e1f4
margdaly <margaret.daly@gmail.com>
2023-08-02 12:42:08 -0600

    test: Add validation test for customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,10 @@
 class Customer < ApplicationRecord
+  validates_with EmailAddress::ActiveRecordValidator, field: :email
+  validates_uniqueness_of :email
+  validates_presence_of :first_name, :last_name, :address, :password_digest, :email
+  
+  has_many :subscriptions
+  has_many :teas, through: :subscriptions
+
+  has_secure_password
 end
