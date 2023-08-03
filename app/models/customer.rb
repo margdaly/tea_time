f6fb538c6d14a53c8b63db93b00e7144317e487a
margdaly <margaret.daly@gmail.com>
2023-08-03 14:10:55 -0600

    fix: Remove validations first,last,&address customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,8 @@
 class Customer < ApplicationRecord
+  has_secure_password
   validates_uniqueness_of :email
-  validates_presence_of :first_name, :last_name, :address, :password_digest, :email
-  
+  validates_presence_of :password_digest, :email
+
   has_many :subscriptions, dependent: :destroy
   has_many :teas, through: :subscriptions
-
-  has_secure_password
 end
